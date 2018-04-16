847b86ac8f125c6cc97fea8470614057a9b6b3ae
Krishna Sharma <skrishna@cs.stonybrook.edu>
2018-04-16 01:34:16 -0400

    mst beginning

---
+++ assignment2/PcwRad.cpp
@@ -0,0 +1,53 @@
+//#include<PcwRad.h>
+#include<math.h>
+#include<stdlib.h>
+#include<stdio.h>
+//
+//void par_PCW_RS(int n, Edges* edges,int noe, int* R)
+//{
+//	//noe = number of edges
+//	int* A = createArr(noe,1);
+//	int k = ceil(log2(noe)) + 1;
+//	int u,j;
+////TODO cilk for
+//	for(int i=0;i<noe;i++)
+//		A[i] = edges[i].u<<k+i;
+//	parRadixSort(A,noe,k+ceil(log2(n)));
+////TODO cilk for
+//	for(int i=0;i<noe;i++)
+//	{
+//		u = A[i]>>k;
+//		j = A[i] - u<<k;
+//
+//	if(i==1||u!=A[i-1]>>k)
+//		R[u] = j;
+//	}
+//}
+struct Edges
+{
+	int u,v;
+};
+void printEdges(Edges* edges,int size)
+{
+	for(int i=0;i<size;i++)
+		printf("\nu=%d v=%d\n",edges[i].u,edges[i].v);
+
+}
+int main(int argc,char* argv[])
+{
+
+	int n,noe;
+	scanf("%d %d",&n,&noe);
+	printf("\nnumber of vertices = %d\nnumber of edges%d",n,noe);
+	Edges* edges = new Edges[n];
+
+	for(int i=0;i<noe;i++)
+		scanf("%d %d",&edges[i].u,&edges[i].v);
+	printEdges(edges,noe);
+	int* R = new int[noe];
+//	par_PCW_RS(n,edges,noe,R);
+
+	return 0;
+}
+
+
