fba6faca97323ec5d2f0e8d336a3139d60c6b5fc
hirenmayani <mayanihiren@gmail.com>
2018-04-16 03:10:06 -0400

    gonna run BS

--- assignment2/CW_BinarySearch.cpp
+++ assignment2/CW_BinarySearch.cpp
@@ -3,25 +3,58 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-void parCW_BS(int n, Edges* edges,int noe, int* R)
+void parCW_BS(int n, E* edges,int noe, int* R)
 {
 	//noe = number of edges
-	int* A = createArr(noe,1);
-	int k = ceil(log2(noe)) + 1;
-	int u,j;
-//TODO cilk for
-	for(int i=0;i<noe;i++)
-		A[i] = edges[i].u<<k+i;
-	parRadixSort(A,noe,k+ceil(log2(n)));
-//TODO cilk for
-	for(int i=0;i<noe;i++)
-	{
-		u = A[i]>>k;
-		j = A[i] - u<<k;
+	int* B = createArr(n,1);
+	int* l = createArr(n,0);
+	int* h = createArr(n,0);
+	int* lo = createArr(n,0);
+	int* hi = createArr(n,0);
+	int* md = createArr(n,0);
 
-	if(i==1||u!=A[i-1]>>k)
-		R[u] = j;
+	int ks = ceil(log2(noe)) + 1;
+
+	cilk_for(int u=0;u<n;u++){
+		l[u]=0;
+		h[u]=noe-1;
 	}
+
+	for(int k=0;k<ks;k++){
+		cilk_for(int u=0;u<n;u++){
+			B[u]=0;
+			lo[u]= l[u];
+			hi[u]=h[u];
+		}
+
+		cilk_for(int i=0;i<noe;i++){
+			int u = E[i].u;
+			md[u] = int(floor((lo[u]+hi[u])/2));
+			if (i>=lo[u] and i<= md[u]){
+				B[u]=1;
+			}
+		}
+
+		cilk_for(int i=0;i<noe;i++){
+			int u = E[i].u;
+			md[u] = int(floor((lo[u]+hi[u])/2));
+			if (B[u]== 1 and i>=lo[u] and i<= md[u]){
+				h[u] = md[u];
+			}else if(B[u]== 0 and i<= hi[u] and i> md[u]){
+				l[u] = md[u]+1;
+			}
+		}
+
+		cilk_for(int i=0;i<noe;i++){
+			int u = E[i].u;
+			if (i == l[u]){
+				R[u]=i;
+			}
+		}
+
+
+	}
+
 }
 //struct Edges
 //{
@@ -33,6 +66,8 @@ void printEdges(Edges* edges,int size)
 		printf("\nu=%d v=%d w=%d\n",edges[i].u,edges[i].v,edges[i].w);
 
 }
+
+
 int main(int argc,char* argv[])
 {
 
