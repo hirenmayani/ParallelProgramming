bd285c94c3fd1360f81e8b22704d1cf851a3690b
Hiren Mayani <tg848375@login2.stampede2.tacc.utexas.edu>
2018-05-03 11:12:29 -0500

    some minor changes

--- assignment3/delme.c
+++ assignment3/delme.c
@@ -1,87 +1,33 @@
 #include "mpi.h"
 #include <stdio.h>
 
-
-int** createMatrix(int size,int init)
-{
-  int i=0;
-  int j=0;
-
-
-  int **mat;
-  mat = (int**)calloc(sizeof(int*),size);
-  for(int i = 0; i < size; i++)
-  {
-      mat[i] = (int*)calloc(sizeof(int), size);
-  }
-
-  if(init == 0)
-  {
-    for(i=0;i<size;i++)
-    {
-    for(j=0;j<size;j++)
-    {
-          mat[i][j] = 0;
-
-        }
-      }
-  }
-  else
-  {
-    for(i=0;i<size;i++)
-    {
-    for(j=0;j<size;j++)
-    {
-          mat[i][j] = rand()%30;
-
-        }
-    }
-  }
-return mat;
-
-}
-
-
-void printMat(int** mat,int size)
-{
-  int i,j;
-
-
-  for(i=0;i<size;i++)
-  {
-    for(j=0;j<size;j++)
-      printf("%d ",mat[i][j]);
-    printf("\n");
-    }
-}
-
- 
 int main(int argc, char *argv[])
 {
 	int myrank, n = 0, p = 4;
 	//int p=4;
 	int r = atoi("3");
 	n = pow(2, r);
-
+	int ii,jj;
+	int proc;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &p);
 	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 	int rootp = sqrt(p * 1.0);
 	MPI_Request sendreq[rootp], recvreq[rootp];
 
-	int i = floor(myrank / rootp);
-	int j = myrank % rootp;
+	//int i = floor(myrank / rootp);
+	//int j = myrank % rootp;
 	int nbrp = n / rootp;
 
-    int A[n*n];
+    	int A[n*n];
 	if (myrank == 0) {
-	        for (int ii=0; ii<n*n; ii++) {
+	        for(ii=0; ii<n*n; ii++) {
 	            A[ii] = ii;
 	        }
 	    }
 
 	    int b[nbrp*nbrp];
-	    for (int ii=0; ii<nbrp*nbrp; ii++)
+	    for(ii=0; ii<nbrp*nbrp; ii++)
 	    		b[ii] = 0;
 
 	    MPI_Datatype blocktype;
@@ -94,31 +40,31 @@ int main(int argc, char *argv[])
 	    int disps[rootp*rootp];
 	    int counts[rootp*rootp];
 
-	    for (int ii=0; ii<rootp; ii++) {
-	        for (int jj=0; jj<rootp; jj++) {
+	    for (ii=0; ii<rootp; ii++) {
+	        for (jj=0; jj<rootp; jj++) {
 	            disps[ii*rootp+jj] = ii*n*nbrp+jj*nbrp;
 	            counts [ii*rootp+jj] = 1;
 	        }
 	    }
 
-	    MPI_Scatterv(A, counts, disps, blocktype, b, nbrp*nbrp, MPI_CHAR, 0, MPI_COMM_WORLD);
+	    MPI_Scatterv(A, counts, disps, blocktype, b, nbrp*nbrp, MPI_INT, 0, MPI_COMM_WORLD);
 
 	    /* each proc prints it's "b" out, in order */
-	    for (int proc=0; proc<p; proc++) {
-	        if (proc == rank) {
-	            printf("Rank = %d\n", rank);
-	            if (rank == 0) {
+	    for (proc=0; proc<p; proc++) {
+	        if (proc == myrank) {
+	            printf("Rank = %d\n", myrank);
+	            if (myrank == 0) {
 	                printf("Global matrix: \n");
-	                for (int ii=0; ii<n; ii++) {
-	                    for (int jj=0; jj<n; jj++) {
-	                        printf("%3d ",(int)a[ii*n+jj]);
+	                for(ii=0; ii<n; ii++) {
+	                    for(jj=0; jj<n; jj++) {
+	                        printf("%3d ",(int)A[ii*n+jj]);
 	                    }
 	                    printf("\n");
 	                }
 	            }
 	            printf("Local Matrix:\n");
-	            for (int ii=0; ii<nbrp; ii++) {
-	                for (int jj=0; jj<nbrp; jj++) {
+	            for(ii=0; ii<nbrp; ii++) {
+	                for(jj=0; jj<nbrp; jj++) {
 	                    printf("%3d ",(int)b[ii*nbrp+jj]);
 	                }
 	                printf("\n");
