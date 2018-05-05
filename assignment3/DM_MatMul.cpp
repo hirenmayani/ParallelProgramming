28ee5bff4175c8f15d885198512a1662f35f5a2f
hirenmayani <mayanihiren@gmail.com>
2018-05-05 02:42:51 -0400

    create arr generates -100 to 100

--- assignment3/DM_MatMul.cpp
+++ assignment3/DM_MatMul.cpp
@@ -313,8 +313,8 @@ int broadcastAbroadcastB(int argc, char* argv[],int r) {
 	int** C;
 	int** At;
 	int** Bt;
-	A = createContMatrix(nbrp, myrank);
-	B = createContMatrix(nbrp, myrank);
+	A = createContMatrix(nbrp, -2);
+	B = createContMatrix(nbrp, -2);
 	C = createContMatrix(nbrp, 0);
 	Bt = createContMatrix(nbrp, 0);
 	At = createContMatrix(nbrp, 0);
@@ -389,8 +389,8 @@ int rotateAbroadcastB(int argc, char* argv[], int r) {
 	int** A;
 	int** B;
 	int** C;
-	A = createContMatrix(nbrp, myrank);
-	B = createContMatrix(nbrp, myrank);
+	A = createContMatrix(nbrp, -2);
+	B = createContMatrix(nbrp, -2);
 	C = createContMatrix(nbrp, 0);
 	int** Bt;
 	Bt = createContMatrix(nbrp, 0);
@@ -484,8 +484,8 @@ int rotateBoth(int argc, char* argv[], int r) {
 	int** A;
 	int** B;
 	int** C;
-	A = createContMatrix(n, myrank);
-	B = createContMatrix(n, myrank);
+	A = createContMatrix(n, -2);
+	B = createContMatrix(n, -2);
 	C = createContMatrix(nbrp, 0);
 
 	int left = (rootp + j - i) % rootp;
@@ -563,8 +563,8 @@ int rotateAB(int argc, char* argv[], int r) {
 	int** A;
 	int** B;
 	int** C;
-	A = createContMatrix(nbrp, myrank);
-	B = createContMatrix(nbrp, myrank);
+	A = createContMatrix(nbrp, -2);
+	B = createContMatrix(nbrp, -2);
 	C = createContMatrix(nbrp, 0);
 
 	int left = (rootp + j - i) % rootp;
